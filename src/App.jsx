c4c298c29a7c9330bda861e16d85cc2b3775aa76
M Taha Makhdoom <taha.makhdoom10@gmail.com>
2024-07-15 14:13:04 +0500

    changed some routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,23 @@ import Signup from './Pages/Signup';
 import Login from './Pages/Login';
 import ProfilePage from './Pages/ProfilePage';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { Route, Routes, useNavigate } from "react-router-dom";
-import { useState,useEffect } from 'react';
+import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 const clientId = '359450186285-snvq9iqkcmddvve4i8eb74qokpueud0o.apps.googleusercontent.com';
 
 
 function App() {
-// const navigate=useNavigate();
-// useEffect(() => {
-//   navigate("/signup");
-// }, []);
+  const navigate=useNavigate();
+
+  let location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      navigate("/signup");
+    }
+  });
+
   const [loginCredentials, setLoginCredentials] = useState({
     id: 0,
     name: "",
@@ -24,7 +30,6 @@ function App() {
   return (
     <GoogleOAuthProvider clientId={clientId}>
       <Routes>
-        <Route path="/signup" element={<Signup loginCredentials={loginCredentials} setLoginCredentials={setLoginCredentials}/>} />
         <Route path="/signup" element={<Signup loginCredentials={loginCredentials} setLoginCredentials={setLoginCredentials}/>} />
         <Route path="/login" element={<Login loginCredentials={loginCredentials} setLoginCredentials={setLoginCredentials}/>} />
         <Route path="/profile" element={<ProfilePage loginCredentials={loginCredentials} setLoginCredentials={setLoginCredentials}/>} />
